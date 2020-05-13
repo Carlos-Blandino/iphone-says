23f61e55118480f64ec6ef47c5ef62dd7a83e27a
Carlos Blandino <cablandino66@gmail.com>
2020-05-13 11:08:08 -0700

    making adjustments to autolayout making it adapt to other screen sizes

--- iPhone-Says/ViewController.swift
+++ iPhone-Says/ViewController.swift
@@ -12,16 +12,17 @@ class ViewController: UIViewController {
     var modelName = UIDevice.current.name
     var askMeImages = [UIImage.init(named: "yes"),UIImage.init(named: "notToday"),UIImage.init(named: "notSure"),UIImage.init(named: "maybe"),UIImage.init(named: "askMeNicely"),UIImage.init(named: "askMeLater"),UIImage.init(named: "askGod"),UIImage.init(named: "notToday")]
     
-    @IBOutlet weak var askMeImageView: UIImageView!
+    @IBOutlet weak var askMeView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        askMeImageView.image =  #imageLiteral(resourceName: "askMe.png")
+        askMeView.image =  #imageLiteral(resourceName: "askMe.png")
     }
     
    
     @IBAction func tryMeButton(_ sender: Any) {
-        askMeImageView.image = askMeImages.randomElement()!!
+        askMeView.image = askMeImages.randomElement()!
     }
     
 }
